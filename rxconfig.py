77f48c3b9e85fc5f02d9e96895d4bfa735bd2978
MaGadev69 <maxigasquez@gmail.com>
2025-05-08 20:33:05 -0300

    Agrego estáticos del frontend

--- rxconfig.py
+++ rxconfig.py
@@ -1,3 +1,6 @@
 import reflex as rx
 
-config = rx.Config(app_name="integracion_minimal_web_page")
+config = rx.Config(
+    app_name="integracion_minimal_web_page",
+    bun={"disable_export_sitemap": True},
+    )
